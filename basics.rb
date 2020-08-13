46762fcf2c43fa10c125d1f13a4646072703a086
Hector Ordonez <hjavierog@gmail.com>
2020-08-13 14:05:35 +0200

    added ladders

--- basics.rb
+++ basics.rb
@@ -12,14 +12,33 @@ while current_position < target_position
 
   # obstacles
   if current_position == 8
-    puts "Oops! Snake in 8, you go to 3"
+    puts "Oops! Snake in #{current_position}, you go to 3"
     current_position = 3
   elsif current_position == 13
-    puts "Oops! Snake in 13, you go to 8"
+    puts "Oops! Snake in #{current_position}, you go to 8"
     current_position = 8
   elsif current_position == 20
-    puts "Oops! Snake in 20, you go to 11"
+    puts "Oops! Snake in #{current_position}, you go to 11"
     current_position = 11
+  elsif current_position == 24
+    puts "Oops! Snake in #{current_position}, you go to 17"
+    current_position = 17
+    # helpers
+  elsif current_position == 1
+    puts "Nice! Ladder in #{current_position}, you go to 9"
+    current_position = 9
+  elsif current_position == 3
+    puts "Nice! Ladder in #{current_position}, you go to 7"
+    current_position = 7
+  elsif current_position == 6
+    puts "Nice! Ladder in #{current_position}, you go to 17"
+    current_position = 17
+  elsif current_position == 12
+    puts "Nice! Ladder in #{current_position}, you go to 18"
+    current_position = 18
+  elsif current_position == 19
+    puts "Nice! Ladder in #{current_position}, you go to 23"
+    current_position = 23
   end
 
   puts "Now position is #{current_position}"
