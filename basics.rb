8356760bdfaad43b2a6c6857c5449ce17376aba0
Hector Ordonez <hjavierog@gmail.com>
2020-08-13 14:30:08 +0200

    optimised

--- basics.rb
+++ basics.rb
@@ -3,6 +3,19 @@ def debug(msg)
 end
 
 def play_game
+  specials = {
+      # snakes
+      8 => 3,
+      13 => 8,
+      20 => 11,
+      24 => 17,
+      # ladders
+      1 => 9,
+      3 => 7,
+      6 => 17,
+      12 => 18,
+      19 => 23
+  }
   roll_sequence = []
 
   current_position = 1
@@ -19,40 +32,10 @@ def play_game
     current_position += roll
 
     # obstacles
-    case current_position
-    when 8
-      debug("Oops! Snake in #{current_position}, you go to 3")
-      current_position = 3
-    when 13
-      debug("Oops! Snake in #{current_position}, you go to 8")
-      current_position = 8
-    when 20
-      debug("Oops! Snake in #{current_position}, you go to 11")
-      current_position = 11
-    when 24
-      debug("Oops! Snake in #{current_position}, you go to 17")
-      current_position = 17
-      # ladders
-    when 1
-      debug("Oops! Snake in #{current_position}, you go to 9")
-      current_position = 9
-    when 8
-      debug("Oops! Snake in #{current_position}, you go to 3")
-      current_position = 3
-    when current_position == 3
-      debug("Nice! Ladder in #{current_position}, you go to 7")
-      current_position = 7
-    when current_position == 6
-      debug("Nice! Ladder in #{current_position}, you go to 17")
-      current_position = 17
-    when current_position == 12
-      debug("Nice! Ladder in #{current_position}, you go to 18")
-      current_position = 18
-    when current_position == 19
-      debug("Nice! Ladder in #{current_position}, you go to 23")
-      current_position = 23
-    else
-      # Nothing special here
+    if specials.has_key? current_position
+      new_position = specials[current_position]
+      debug("Special: from #{current_position}, to #{new_position}")
+      current_position = new_position
     end
 
     debug("Now position is #{current_position}")
