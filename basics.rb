52e6d836faa45de5e40402478d03b77e758b26b4
Hector Ordonez <hjavierog@gmail.com>
2020-08-13 13:57:31 +0200

    Rolling dices until I get to target positino

--- basics.rb
+++ basics.rb
@@ -1,6 +1,13 @@
-roll = 0
+current_position = 1
+target_position = 25
 
-while roll != 6
+roll = 0
+while current_position < target_position
+  # throwing dice
   roll = rand(1..6)
-  puts "throw : #{roll}"
+
+  # adding to current position
+  current_position += roll
+
+  puts "throw : #{roll} and now position is #{current_position}"
 end
