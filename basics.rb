5ef4c6819f980947c8f347f172a98f234e233bc8
Hector Ordonez <hjavierog@gmail.com>
2020-08-13 14:01:18 +0200

    Added snakes

--- basics.rb
+++ basics.rb
@@ -5,9 +5,22 @@ roll = 0
 while current_position < target_position
   # throwing dice
   roll = rand(1..6)
+  puts "throw : #{roll}"
 
   # adding to current position
   current_position += roll
 
-  puts "throw : #{roll} and now position is #{current_position}"
+  # obstacles
+  if current_position == 8
+    puts "Oops! Snake in 8, you go to 3"
+    current_position = 3
+  elsif current_position == 13
+    puts "Oops! Snake in 13, you go to 8"
+    current_position = 8
+  elsif current_position == 20
+    puts "Oops! Snake in 20, you go to 11"
+    current_position = 11
+  end
+
+  puts "Now position is #{current_position}"
 end
