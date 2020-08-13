712a17c5f36c9327b12bd103290418f9f8c41329
Hector Ordonez <hjavierog@gmail.com>
2020-08-13 15:07:01 +0200

    Using a function to print

--- matrix.rb
+++ matrix.rb
@@ -2,17 +2,17 @@ def debug(msg)
   puts msg
 end
 
+def iterate(matrix, col, row)
+  debug(matrix[col][row])
+end
 
 matrix = [
-    1..5,
-    6..10,
-    7..15,
-    8..20,
-    9..25,
+    # [1..5], shouldn't this work?
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25],
 ]
 
-matrix.each do |row|
-  row.each do |n|
-    debug(n)
-  end
-end
+iterate(matrix, 1, 0)
